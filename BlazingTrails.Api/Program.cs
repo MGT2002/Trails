07850ec8cb8149df4de09c445437dd21f7181f61
GarikMGT <garik.mikayelyan@factumsoft.com>
2024-09-30 18:54:30 +0400

    Page 150.

--- BlazingTrails.Api/Program.cs
+++ BlazingTrails.Api/Program.cs
@@ -1,4 +1,6 @@
 using BlazingTrails.Api.Persistence;
+using BlazingTrails.Shared.Features.ManageTrails;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,7 @@ builder.Services.AddDbContext<BlazingTrailsContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString(nameof(BlazingTrailsContext)))
 );
 builder.Services.AddControllers();
+builder.Services.AddValidatorsFromAssembly(typeof(TrailDto).Assembly);
 
 var app = builder.Build();
 
