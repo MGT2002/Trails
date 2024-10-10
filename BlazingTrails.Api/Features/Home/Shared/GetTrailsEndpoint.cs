5adb9233bd2f80a7f70068893802374424458d7b
GarikMGT <garik.mikayelyan@factumsoft.com>
2024-10-10 20:55:38 +0400

    Appendix B.

---
+++ BlazingTrails.Api/Features/Home/Shared/GetTrailsEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.ApiEndpoints;
+using BlazingTrails.Api.Persistence;
+using BlazingTrails.Shared.Features.Home.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingTrails.Api.Features.Home.Shared;
+
+public class GetTrailsEndpoint(BlazingTrailsContext context) : EndpointBaseAsync
+    .WithRequest<int>
+    .WithActionResult<GetTrailsRequest.Response>
+{
+    private readonly BlazingTrailsContext context = context;
+
+    [HttpGet(GetTrailsRequest.RouteTemplate)]
+    public override async Task<ActionResult<GetTrailsRequest.Response>> HandleAsync(int request, CancellationToken cancellationToken = default)
+    {
+        var trails = await context.Trails
+            .Include(x => x.Route)
+            .ToListAsync(cancellationToken);
+
+        var response = new GetTrailsRequest.Response(trails.Select(trail => new GetTrailsRequest.Trail(
+            trail.Id,
+            trail.Name,
+            trail.Image,
+            trail.Location,
+            trail.TimeInMinutes,
+            trail.Length,
+            trail.Description
+            )));
+
+        return Ok(response);
+    }
+}
