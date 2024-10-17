418bb047499c4722acd14fb7f21bc207e64c8930
GarikMGT <garik.mikayelyan@factumsoft.com>
2024-10-17 21:36:55 +0400

    Page 247.

--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -14,4 +14,5 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
+//builder.Services.AddOidcAuthentication();
 await builder.Build().RunAsync();
