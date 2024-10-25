6b9272e0585f811ed4e5dfb00a83ffe549cbe194
GarikMGT <garik.mikayelyan@factumsoft.com>
2024-10-25 21:24:09 +0400

    amend

--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -1,6 +1,7 @@
 using BlazingTrails.Client;
 using BlazingTrails.Client.Features.ManageTrails.AddTrail;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -14,10 +15,11 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
-builder.Services.AddOidcAuthentication(options =>
+builder.Services.AddAuth0Authentication(options =>
 {
-    options.ProviderOptions.Authority = "https://dev-8b2sv13c44xprhww.us.auth0.com";
-    options.ProviderOptions.ClientId = "i0ckruXVm4GPWK2k0tYiozMOgMf6nVqk";
-    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.Authority = builder.Configuration["Auth0:Authority"];
+    options.ProviderOptions.ClientId = builder.Configuration["Auth0:ClientId"];
+    options.ProviderOptions.RedirectUri = builder.Configuration["Auth0:RedirectUri"];
 });
+
 await builder.Build().RunAsync();
