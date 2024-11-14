29466e7e4c47085fdd2edf4174eb9d9ffcb8aa7c
GarikMGT <garik.mikayelyan@factumsoft.com>
2024-11-14 21:24:49 +0400

    Page 279.

--- BlazingTrails.Client/State/AppState.cs
+++ BlazingTrails.Client/State/AppState.cs
@@ -1,11 +1,6 @@
-﻿using BlazingTrails.Shared.Features.ManageTrails.Shared;
-
-namespace BlazingTrails.Client.State;
+﻿namespace BlazingTrails.Client.State;
 
 public class AppState
 {
-    private TrailDto unsavedNewTrail = new();
-    public TrailDto GetTrail() => unsavedNewTrail;
-    public void SaveTrail(TrailDto trail) => unsavedNewTrail = trail;
-    public void ClearTrail() => unsavedNewTrail = new();
+    public NewTrailState NewTrailState { get; } = new();
 }
